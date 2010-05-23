acba89c7d9148cb724ef0c39d5f4c3845ae3928d
Phil Adams <phil@philadams.net>
2010-05-23 15:27:50 -0400

    simple rsync-based backup working

---
+++ backup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+convenience script to backup various folders to an external hd via rsync
+"""
+
+import subprocess
+
+EXTERNAL_PATH = '/Volumes/Calvin/Backup/current/'
+TO_SYNC = ['/Users/phil/Code', # no trailing slash
+           '/Users/phil/Documents',
+           '/Users/phil/Movies',
+           '/Users/phil/Music',
+           '/Users/phil/Pictures',
+           '/Users/phil/Sites',
+           '/Users/phil/todo.txt']
+CMD = ['rsync', '-a', '--delete-after', '--exclude=.DS_Store']
+
+print('syncing folders:')
+for local_dir in TO_SYNC:
+    print('\t%s...' % local_dir)
+    retcode = subprocess.call(CMD + [local_dir, EXTERNAL_PATH])
+print('sync complete')
